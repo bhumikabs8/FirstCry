58e3db9aab38c9d9dcbc06a733fca7221416a6a7
bhumikabs8 <102541920+bhumikabs8@users.noreply.github.com>
2023-09-15 17:41:50 +0530

    MyProfile Scenario

---
+++ FirstCry/TestRunner.java
@@ -0,0 +1,14 @@
+package mphasis.FirstCry.testSteps;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features = "Features//myprofile.feature",
+		glue = "mphasis.FirstCry.testSteps"
+		)
+
+public class TestRunner extends AbstractTestNGCucumberTests
+{
+	
+}
